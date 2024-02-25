41bb6bd3f01b0ed0cbb846f29ee7acc0ffc1d657
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2024-02-25 08:10:53 +0100

    cambios en testimoniales

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,4 +1,5 @@
 import { Viaje } from '../models/viaje.js';
+import { Testimonial } from '../models/Testimoniales.js';
 
 const paginaInicio = (req, res) => {
 	// req - lo que enviamos : res - lo que express nos responde
@@ -20,10 +21,16 @@ const paginaViajes = async (req, res) => {
 		viajes, // llave y valor(nombre base de datos) se llaman igual
 	});
 };
-const paginaTestimoniales = (req, res) => {
-	res.render('testimoniales', {
-		pagina: 'Testimoniales',
-	});
+const paginaTestimoniales = async (req, res) => {
+	try {
+		const testimoniales = await Testimonial.findAll();
+		res.render('testimoniales', {
+			pagina: 'Testimoniales',
+			testimoniales,
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // Muestra un viaje por su slug
--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,18 +1,49 @@
-const guardarTestimonial = (req, res) => {
+import { Testimonial } from '../models/Testimoniales.js';
+
+const guardarTestimonial = async (req, res) => {
 	// Validar el formulario
 	const { name, email, mensaje } = req.body;
 
 	const errores = [];
 
-	if (name.trim() === '' || email.trim() === '' || mensaje.trim() === '') {
-		errores.push({ mensaje: 'campos Vacios' });
+	if (name.trim() === '') {
+		errores.push({ mensaje: 'El nombre está vacio' });
+	}
+
+	if (email.trim() === '') {
+		errores.push({ mensaje: 'El correo está vacio' });
+	}
+
+	if (mensaje.trim() === '') {
+		errores.push({ mensaje: 'El mensaje está vacio' });
 	}
 
 	if (errores.length > 0) {
+		// Consultar Testimoniales Existentes
+		const testimoniales = await Testimonial.findAll();
+
 		// Mostrar la vista con errores
 		res.render('testimoniales', {
-			mensaje: errores,
+			pagina: 'Testimoniales',
+			testimoniales,
+			errores,
+			name,
+			email,
+			mensaje,
 		});
+	} else {
+		// Almacenarlo en la BD
+
+		try {
+			await Testimonial.create({
+				nombre: name,
+				correo: email,
+				mensaje,
+			});
+			res.redirect('/testimoniales');
+		} catch (error) {
+			console.log(error);
+		}
 	}
 };
 
