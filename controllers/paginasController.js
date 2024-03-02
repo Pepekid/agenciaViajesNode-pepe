61c7fc7eda5182e9e9f85eaa0f04a84e032fc61c
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2024-03-02 08:48:33 +0100

    agregado testimoniales a inicio

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,15 +1,21 @@
 import { Viaje } from '../models/viaje.js';
 import { Testimonial } from '../models/Testimoniales.js';
 
+// req - lo que enviamos : res - lo que express nos responde
 const paginaInicio = async (req, res) => {
 	// Consultar 3 viajes del modelo Viaje
+
+	const promiseDB = [];
+	promiseDB.push(Viaje.findAll({ limit: 3 }));
+	promiseDB.push(Testimonial.findAll({ limit: 3 }));
+
 	try {
-		const viajes = await Viaje.findAll({ limit: 3 });
-		// req - lo que enviamos : res - lo que express nos responde
+		const resultado = await Promise.all(promiseDB);
 		res.render('inicio', {
 			pagina: 'Inicio',
 			clase: 'home',
-			viajes,
+			viajes: resultado[0],
+			testimoniales: resultado[1],
 		});
 	} catch (error) {
 		console.log(error);
